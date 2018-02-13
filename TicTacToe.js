0cdad88986a249de925ae9b2e3a53e49c150136b
Alyssa Keimach <alyssakeimach@gmail.com>
2018-02-13 12:38:38 -0800

    rewrite with prompt-grid npm module

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,38 +1,82 @@
+const Prompt = require('prompt-grid');
 
 class Game { // first thing because not hoisted
 
-  constructor(row, col) {
-    this.row = row;
-    this.col = col;
-    this.maxPlays = row * col;
-    this.board = [];
-    for (let c = 0; c < col; c++) {
-      let emptyRow = [];
-      for (let r = 0; r < row; r++) {
-        emptyRow.push(' ');
+  constructor(size) {
+    this.size = size;
+    this.maxPlays = size * size;
+    this.currentPlay = 0;
+    this.boardSquare = [];
+    this.boardFlat = [];
+    for (let r = 0; r < size; r++) { // number of rows
+      let row = [];
+      for (let c = 0; c < size; c++) { // for each column
+        const piece = {
+          name: ' ',
+          value: 0
+        };
+        row.push(piece);
+        this.boardFlat.push(piece);
       }
-      this.board.push(emptyRow);
+      this.boardSquare.push(row);
+    }
+    this.boardSquare[2][2].name = 'x';
+    this.boardSquare[2][2].value = 1;
+    this.boardSquare[1][2].name = 'o';
+    this.boardSquare[1][2].value = -1;
+  }
+
+  boardState() {
+    for (let r = 0; r < this.size; r++) { // for each row
+      for (let c = 0; c < this.size; c++) {
+        if (c > 0 && c < this.size) process.stdout.write(' | ');
+        process.stdout.write(this.boardSquare[r][c].name);
+      }
+      (r < (this.size - 1)) ? (console.log('\n' + ('---').repeat(this.size))) : (console.log());
     }
   }
 
-  get boardState() {
-    for (let r = 0; r < this.row; r++) { // for each row
-      for (let c = 0; c < this.col; c++) {
-        if (c > 0 && c < this.col) process.stdout.write(' | ');
-        process.stdout.write(this.board[r][c]);
+  makePlay() {
+    const prompt = new Prompt({
+      name: 'board',
+      choices: this.boardFlat
+    });
+
+    prompt.run().then(answer => {
+      // console.log(prompt.options.choices.choices);
+      const position = prompt.options.choices.position;
+      // the position doesn't have a value yet
+      if (this.boardFlat[position].value === 0) {
+        this.boardFlat[position].value = ((this.currentPlay % 2) === 0) ? -1 : 1;
+        this.boardFlat[position].name = ((this.currentPlay % 2) === 0) ? 'o' : 'x';
+        game.currentPlay++;
+
+        let counter = 0;
+        for (let r = 0; r < this.size; r++) {
+          for (let c = 0; c < this.size; c++) {
+            this.boardSquare[r][c] = this.boardFlat[counter];
+            counter++;
+          }
+        }
+        this.boardState();
+        for (let r = 0; r < this.size; r++) {
+          for (let c = 0; c < this.size; c++) {
+            this.boardSquare[r][c] = this.boardFlat[counter];
+            counter++;
+          }
+        }
       }
-      (r < (this.row - 1)) ? (console.log('\n' + ('---').repeat(this.col))) : (console.log());
-    }
+      else {
+        this.makePlay();
+      }
+    });
   }
 }
 
 
-const game = new Game(4, 4);
-// for (let i = 0; i < game.maxPlays; i++) {
-  game.boardState;
-  // game.makePlay;
-  // game.checkWin;
-// }
+const game = new Game(3);
+
+game.makePlay();
 
 
 
