758d5488ebc2c751f0a7eee039771bb385473edc
Alyssa Keimach <alyssakeimach@gmail.com>
2018-02-12 22:39:49 -0800

    refactor with ternary

--- number_words.js
+++ number_words.js
@@ -16,36 +16,33 @@ intToWord = (integerIn) => {
 
   while (input > 0) {
 
-    if ((input % 1000) > 0) { // if the hundreds/tens/one place is non-zero
+    const hundsPlace = input % 1000;
 
-      if ((input % 100) < 20) { // if theres a number like "twelve" or "ninteen"
-        output = numWord.digits[input % 100] + numWord.beyond[iter] + output;
-      }
-      else { // build the tens, then ones, then 
-        output = numWord.tens[Math.floor((input % 100) / 10)] + numWord.digits[input % 10] + numWord.beyond[iter] + output;
-      }
-      if ((input % 1000) < 100) {
-        output = numWord.digits[Math.floor((input % 1000) / 100)] + output;
-      }
-      else {
-        output = numWord.digits[Math.floor((input % 1000) / 100)] + numWord.beyond[1] + output;
-      }
+    if (hundsPlace > 0) { // if the current section is non-zero (ex. 89,XXX,910)
+
+      const tensPlace = hundsPlace % 100;
+
+      output = numWord.digits[Math.floor(hundsPlace / 100)] + // digit in the hundreds place
+          ((hundsPlace > 100) ? (numWord.beyond[1]) : ("")) + // add the word "hundred" or not
+          ((tensPlace < 20) ? (numWord.digits[tensPlace]) : // add a weird word like "twelve" or...
+            (numWord.tens[Math.floor(tensPlace / 10)] + numWord.digits[hundsPlace % 10])) + // build the normal tens + ones word
+          numWord.beyond[iter] + output; // add quantifier like "million" depending on iteration number
+
     }
 
-    if ((iter === 0) && (input > 100)) { iter++; }
+    if ((iter === 0) && (input > 100)) iter++;
     iter++;
-    input = Math.floor(input / 1000);
+    input = Math.floor(input / 1000); // get the next section of 3 digits
   }
-  if (integerIn < 0) {
-    output = " negative" + output;
-  }
-  return output;
+  return ((integerIn < 0) ? (" negative" + output) : (output));
 };
 
 console.log("0 :" + intToWord(0));
 console.log("1 :" + intToWord(1));
 console.log("022 :" + intToWord(022));
 console.log("18 :" + intToWord(18));
+console.log("-18 :" + intToWord(-18));
+console.log("124312 :" + intToWord(124312));
 
 
 
